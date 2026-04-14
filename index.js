6c5a2b46c653afd0aba40a89f3626438d516925d
uguribadov <uguribadov2012@gmail.com>
2026-04-14 20:19:34 +0400

    index.js

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-let friends =["Ali", "Muhammed", "Eldar"];
+console.log("==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==");
+
+let friends = ["Ali", "Muhammed", "Eldar"];
 friends.unshift("Suleyman")
 friends.pop()
 console.log(friends);
+
+console.log("==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==");
 
 let arr = [1, 2, 3];
 arr.push(4)
 arr.pop()
 console.log(arr);
 
+console.log("==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==");
+
 let queue = ["Ali", "Veli", "Aysel"];
 queue.shift()
 queue.unshift("Orxan")
 console.log(queue);
 
+console.log("==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==");
